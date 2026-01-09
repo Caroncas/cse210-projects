3527947d7bb11339f150988012f457166d74b3a0
Caroncas <caroncas000@gmail.com>
2026-01-08 20:39:01 -0700

    Fixed the things I removed and completed the 1st exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string given = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string surname = Console.ReadLine();
+
+        Console.Write($"Your name is {surname}, {given} {surname}.");
     }
 }
